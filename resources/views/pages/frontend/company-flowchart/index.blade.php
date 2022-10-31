07acdd7b9e36725f6b2b81e8177c3a3c7a675c1a
afsana1996sultana <afsana1996sultana@gmail.com>
2022-10-31 12:15:59 +0600

    Monday Commit

--- resources/views/pages/frontend/company-flowchart/index.blade.php
+++ resources/views/pages/frontend/company-flowchart/index.blade.php
@@ -1,11 +1,11 @@
 @extends('layout.frontend.master')
 @section('main_content')
 <style>
-    .company-flowchart img{
-     height: 667px;
-     width: 1000px;
-    }
-</style>
+ .customer-service-content{
+    padding-left: 5px;
+    padding-top: 70px;
+}
+</style> 
 <div class="page-title-area">
     <div class="container">
         <div class="page-title-content">
@@ -20,10 +20,25 @@
 
 <section class="categories-banner-area">
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-12 col-md-12">
+        <div class="row pt-5 pb-5">
+            <div class="col-lg-7 col-md-10">
                 <div class="company-flowchart">
                     <img src="{{url('frontend/assets/img/categories/organogram.png')}}" alt="image">
+                </div>
+            </div>
+
+            <div class="col-lg-5 col-md-8">
+                <div class="customer-service-content">
+                <h3><i class='bx bx-sync'></i>Our Flowchart</h3> 
+                <p>A process flowchart is a graphical representation of a business process through a flowchart. 
+                It's used as a means of getting a top-down understanding of how a process works.</p>
+                    <ul>
+                        <li>The Process Flowchart or Communication Flow Chart.</li>
+                        <li>The Workflow Chart or Workflow Diagram.</li>
+                        <li>The Swimlane Flowchart (Describe How Separate Departments, Processes or Employees Interact).</li>
+                        <li>The Data Flowchart (Where Data Flows In and Out of an Information System with a Data Flow Diagram).</li>
+                        <li>Flowcharts in Manufacturing (Visualizing process flows is extremely valuable in manufacturing, where standardization and uniformity are important).</li>
+                    </ul>
                 </div>
             </div>
         </div>
--- resources/views/pages/frontend/service/index.blade.php
+++ resources/views/pages/frontend/service/index.blade.php
@@ -1,5 +1,15 @@
 @extends('layout.frontend.master')
 @section('main_content')
+<style>
+.customer-service-area {
+    padding-top: 50px;
+    padding-bottom: 20px;
+}
+
+.customer-service-content{
+
+}
+</style>
 <div class="page-title-area">
     <div class="container">
         <div class="page-title-content">
@@ -11,5 +21,68 @@
         </div>
     </div>
 </div>
+
+<section class="customer-service-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-12">
+                <div class="customer-service-content">
+                    <h3><i class='bx bx-sync'></i>Sourcing/Marketing</h3>
+                    <p>Experienced sourcing teams are constantly on the look out for new products. These teams of young. talented and experienced professionals are sourcing for specialized factories with vertical set up for All possible Products.</p>
+                    <p>Raw materials for production are also sourced locally and internationally from countries such as China, Hong Kong, & Taiwan.</p>
+                    <p>It is insured that raw materials for production are sourced from Oekotex certified factories.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-md-12">
+                <div class="single-categories-box">
+                    <img src="{{url('frontend/assets/img/categories/mission2.webp')}}" alt="image">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="customer-service-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-12">
+                <div class="single-categories-box">
+                    <img src="{{url('frontend/assets/img/categories/mission2.webp')}}" alt="image">
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-md-12">
+                <div class="customer-service-content">
+                    <h3><i class='bx bx-sync'></i>Development</h3>
+                    <p>In-house development teams are precisely focused on new product developments using local imported materials. The development teams keep themselves updated with new and current fashion trends.</p>
+                    <p>Besides this we are also developing samples and products as per customer’s requirement and specification.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="customer-service-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-12">
+                <div class="customer-service-content">
+                    <h3><i class='bx bx-sync'></i>Merchandising</h3>
+                    <p>Each customer is also provided with selected and separate production team (quality controllers). These teams along with the merchandising team performs quality check on the merchandise during production.</p>
+                    <p>Workmanship and other standards such as rubbing test, shrinkage test, color fastness are constantly checked and recorded and analyzed.</p>
+                    <p>Immediate corrective measures are taken if any deviations are found.</p>
+                    <p>Testings are done strictly with certified test laboratory, such as, SGS, BV, Hohenstein, Tuv and any other testing lab as required by the customer.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-md-12">
+                <div class="single-categories-box">
+                    <img src="{{url('frontend/assets/img/categories/mission2.webp')}}" alt="image">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 @endsection
